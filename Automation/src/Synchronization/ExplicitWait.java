1bdfc7fdb010621fb784eba37c1eb22f2f0e75fe
shreya0909sinha <141677699+shreya0909sinha@users.noreply.github.com>
2026-02-19 11:40:50 +0530

    Update ExplicitWait.java

--- Automation/src/Synchronization/ExplicitWait.java
+++ Automation/src/Synchronization/ExplicitWait.java
@@ -13,7 +13,7 @@ public class ExplicitWait {
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		driver.findElement(By.name("username")).sendKeys("Admin");
